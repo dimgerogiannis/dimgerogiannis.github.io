1f165ed237cc020b7d3e15658ed6365b3525e020
Dimitris Gerogiannis <58883344+dimgerogiannis@users.noreply.github.com>
2025-02-15 01:32:56 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://dimgerogiannis.github.io',
+  base: 'dimgerogiannis.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
