f9fb4cff48934fe9ddc027575cc6460e4effb950
Dimitris Gerogiannis <58883344+dimgerogiannis@users.noreply.github.com>
2026-02-04 17:06:34 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: 'https://dimgerogiannis.github.io',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), tailwind()]
 });
